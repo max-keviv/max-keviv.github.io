cd333e48b112c0c5128947dec03f2f2cac24f68e
Vivek Vishal <btk.vivekvishal@gmail.com>
2025-07-26 16:35:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
